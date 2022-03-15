0428906ec79590a196165b30df9babcbc188e579
Suély Antunes <suely.antunes@fire-engine-red.com>
2022-03-15 11:19:42 -0300

    :tada: init project

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,8 +1,8 @@
 export { default as MyButton } from './atoms/Button/Button.vue'
 
-export { default as MyHome } from './atoms/Home/Home.vue'
+export { default as MyHome } from './layouts/Home/Home.vue'
 
-export { default as MyCard } from './atoms/Card/Card.vue'
-export { default as MyPagination } from './atoms/Pagination/Pagination.vue'
+export { default as MyCard } from './molecules/Card/Card.vue'
+export { default as MyPagination } from './molecules/Pagination/Pagination.vue'
 
-export { default as MyNavbar } from './atoms/Navbar/Navbar.vue'
+export { default as MyNavbar } from './organisms/Navbar/Navbar.vue'
