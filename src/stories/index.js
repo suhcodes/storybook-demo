05f1a88441956674ffc6fc966903d5ff19200d3a
Suély Antunes <suely.antunes@fire-engine-red.com>
2022-03-15 11:15:03 -0300

    :tada: init project

---
+++ src/stories/index.js
@@ -0,0 +1,8 @@
+export { default as MyButton } from './atoms/Button/Button.vue'
+
+export { default as MyHome } from './atoms/Home/Home.vue'
+
+export { default as MyCard } from './atoms/Card/Card.vue'
+export { default as MyPagination } from './atoms/Pagination/Pagination.vue'
+
+export { default as MyNavbar } from './atoms/Navbar/Navbar.vue'
